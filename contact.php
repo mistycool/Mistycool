336de269e7478918a33f38c459a12494d90ca488
mistycool <mistycool666@gmail.com>
2025-01-02 08:28:12 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
+    $to = "mistycool666@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send the message.";
+    }
+}
+?>
